b3acee58c54096494ac352563643f85b0d6b8cdc
mzwae <mrmonjaie@gmail.com>
2018-05-28 16:32:32 +0100

    added jobs view

---
+++ application/views/jobs/view.php
@@ -0,0 +1,36 @@
+<div class="page-header">
+  <h1>
+    <?php echo form_open('jobs/index'); ?>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="input-group">
+          <input type="text" class="form-control" name="search_string" placeholder="Search Jobs...">
+          <span class="input-group-btn">
+            <button type="submit" class="btn btn-default">Search</button>
+          </span>
+        </div>
+      </div>
+    </div>
+    <?php echo form_close(); ?>
+  </h1>
+</div>
+
+<table class="table table-hover">
+  <?php foreach($query->result() as $row): ?>
+    <tr>
+      <td>
+        <a href="jobs/apply/<?=$row->job_id?>"><?=$row->job_title?></a>
+        <br>
+        <?php echo word_limiter($row->job_desc, 50); ?>
+      </td>
+      <td>
+        Posted on <?=$row->job_created_at?>
+        <br>
+        Rate is &pound;<?=$row->job_rate?>
+      </td>
+      <td>
+        <a href="jobs/apply/<?=$row->job_id?>">Apply</a>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+</table>
